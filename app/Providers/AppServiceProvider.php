b4fd6a4551b9538c953e6ee92184f32757f319e9
ihdarkaluku0@gmail.com <ihdarkaluku0@gmail.com>
2025-12-25 01:41:29 +0900

    baru saja

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        
+         if ($this->app->environment('production')) {
+        \URL::forceScheme('https');
+    }
         Paginator::useTailwind();
     }
 
