e8b0d090971f768f1d6ce078f7ad54c30c744905
ihdarkaluku0@gmail.com <ihdarkaluku0@gmail.com>
2025-12-25 02:30:41 +0900

    deploy laravel to railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-         if ($this->app->environment('production')) {
-        \URL::forceScheme('https');
-    }
+      
         Paginator::useTailwind();
     }
 
